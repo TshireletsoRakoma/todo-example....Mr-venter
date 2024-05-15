cf072c3197e2776f0029926ff18ec24126d1098a
TshireletsoRakoma <156686384+TshireletsoRakoma@users.noreply.github.com>
2024-05-15 19:31:36 +0200

    added new variable

--- scripts.js
+++ scripts.js
@@ -1,18 +1,50 @@
 // @ts-check
 
 import { state } from "./state.js";
-console.log("It works");
 
-const list =document.querySelector('[data-list]')
-const isHtmlElement = list instanceof HTMLElement;
+// console.log("It works");
+// const addTaskToHtml = (id)  => {
 
-if (!isHtmlElement) {
-    throw new Error('"data-list" attribute not found in HTML');
-}
+// }
+// const list =document.querySelector('[data-list]')
+// const isHtmlElement = list instanceof HTMLElement;
 
-const preview = document.createElement('li');
-preview.className = "task";
+// if (!isHtmlElement) {
+//     throw new Error('"data-list" attribute not found in HTML');
+// }
+
+// const preview = document.createElement('li');
+// preview.className = "task";
+// preview.dataset.task = id;
+
 
 preview.innerHTML = /*html */ `
 
+<label class="task__check">
+<input class="task__input" type="checkbox" disabled/>
+</label>
+<button class="task__title" disabled></button>
+<label class="task__check">
+<svg
+  class="task__icon"
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 96 960 960"
+  style="display:none"
+>
+  <path
+    d="M253 961q-40.212 0-67.606-27.1Q158 906.8 158 867V314h-58v-94h231v-48h297v48h232v94h-58v553q0 39.05-27.769 66.525Q746.463 961 707 961H253Zm454-647H253v553h454V314ZM354 789h77V390h-77v399Zm175 0h78V390h-78v399ZM253 314v553-553Z"
+  ></path>
+</svg>
+</label>
 `;
+
+list.appendChild(preview);
+//};
+
+const updateHtmlTask = (id, changes) => {
+
+};
+
+addTaskToHtml("test");
+addTaskToHtml("test");
+
