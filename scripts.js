8a2608cdb8a504e534fe86d54bab6afe32127402
TshireletsoRakoma <156686384+TshireletsoRakoma@users.noreply.github.com>
2024-05-15 16:51:54 +0200

    Update index.html to use a data attribute for the task list and add a preview task to scripts.js
    The index.html file has been updated to use a data attribute for the task list and to remove the hardcoded task.

--- scripts.js
+++ scripts.js
@@ -2,3 +2,17 @@
 
 import { state } from "./state.js";
 console.log("It works");
+
+const list =document.querySelector('[data-list]')
+const isHtmlElement = list instanceof HTMLElement;
+
+if (!isHtmlElement) {
+    throw new Error('"data-list" attribute not found in HTML');
+}
+
+const preview = document.createElement('li');
+preview.className = "task";
+
+preview.innerHTML = /*html */ `
+
+`;
